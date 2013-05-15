9b0d22a7605cd1c67f01b64c9c552ac52b9de221
root <root@muntanyeta.upc.edu>
2013-05-15 18:23:40 +0200

    fix

--- loadtest/monitor.py
+++ loadtest/monitor.py
@@ -14,7 +14,7 @@ print command
 
 proc = subprocess.Popen(command.split(' '), bufsize=1,  stdout=subprocess.PIPE)
 
-pids = {}
+pids_stats = {}
 
 while True:
     line = proc.stdout.readline()
@@ -22,11 +22,12 @@ while True:
     try:
         ff = re.findall(r'[^\s]+', line)
         if ff[0] in pids:
-            pids.setdefault(ff[0], {'max': 0, 'average': None})
-            if pids[pid]['min'] = None
+            #pids.setdefault(ff[0], {'max': 0, 'average': None})
+            #if pids[pid]['min'] = None
             pid = ff[0]
             res = ff[5]
             cpu = ff[8]
+        
             print '%s MEM: %s CPU: %s' % (pid, res, cpu)
     except:
         pass
